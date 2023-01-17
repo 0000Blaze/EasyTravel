ecf697af7417da9a5deb11891c870b2325c7a688
0000Blaze <rchhetry5@gmail.com>
2023-01-17 19:40:30 +0545

    modal form added

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@ function openNav() {
   document.getElementById("myNav").classList.toggle("menu_width");
   document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style");
 }
+
+$("#myModal").on("shown.bs.modal", function () {
+  $("#myInput").trigger("focus");
+});
